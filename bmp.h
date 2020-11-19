35aa75bb90d36f7338515700099de99bf2d58964
ccosta01 <63289532+ccosta01@users.noreply.github.com>
2020-11-20 00:37:56 +0200

    Update bmp.h
    Minor changes

--- bmp.h
+++ bmp.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
 //structure to hold all necessary image data
-struct image {
+typedef struct{
     unsigned char *pixel_array;
     int padding;
     int pixel_array_size;
@@ -12,19 +12,19 @@ struct image {
 } IMAGE;
 
 //print out pixel data to std out omitting padded bytes
-extern void print_pixel_array(struct image *img);
+extern void print_pixel_array(IMAGE *img);
 
 //convert the pixel array in-place from blue, green, red colour space, to the red, green, blue colour space
-extern void convert_to_rgb(struct image *img);
+extern void convert_to_rgb(IMAGE *img);
 
 //this reverses the pixel array because BMP files by definition are stored upside down
-extern void reverse_pixel_array(struct image *img);
+extern void reverse_pixel_array(IMAGE *img);
 
 //load bmp file image data into a struct
-extern struct image* load_bmp(const char *file_name);
+extern IMAGE* load_bmp(const char *file_name);
 
 //free the memory used by the images pixel data
-void free_img(unsigned char *pixel_array, struct image *img);
+void free_img(unsigned char *pixel_array, IMAGE *img);
 
 //get the colour depth for the currently loaded image as integer.
-extern uint16_t get_colour_depth(struct image *img);
+extern uint16_t get_colour_depth(IMAGE *img);
