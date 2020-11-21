647c9f20153c9d587c7b1872622c5ffedc5ee108
ccosta01 <ccosta01@ucy.ac.cy>
2020-11-21 21:40:10 +0200

    Now the decoding is working

---
+++ cmake-build-debug/CMakeFiles/Homework4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework4.dir/Waterproof.c.o"
+  "CMakeFiles/Homework4.dir/bmp.c.o"
+  "Homework4.exe"
+  "Homework4.pdb"
+  "libHomework4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Homework4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
