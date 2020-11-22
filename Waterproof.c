61ec221b9327f92a35a659ca6ccb0627820e31e4
apatti01 <apatti01@ucy.ac.cy>
2020-11-22 13:46:29 +0200

    Added leitourgia 1. Not working yet.

--- Waterproof.c
+++ Waterproof.c
@@ -181,7 +181,14 @@ void decode_image(IMAGE *img,unsigned int proof_len){
     fclose(file);
 }
 
+
+
 int main(){
+    IMAGE *test1 =  load_bmp("4x3.bmp");
+    print_information(test1);
+    IMAGE *test2 =  load_bmp("image2.bmp");
+    print_information(test2);
+
     IMAGE *cover =  load_bmp("IMG_6865.bmp");
     IMAGE *secret =  load_bmp("IMG_6875.bmp");
     change_pixels(cover,secret,4);
