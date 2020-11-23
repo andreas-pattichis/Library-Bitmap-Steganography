25edb9cc9ee042b33c9fda69c78b9f2f870cf650
ccosta01 <ccosta01@ucy.ac.cy>
2020-11-23 02:04:05 +0200

    So i think that putting text in an image is working okay and reading text from an image should as well work okay but something is wrong and i need tomorrow to figure it out.

--- Waterproof.c
+++ Waterproof.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <string.h>
 
+#define MAX_WORDS 5000
+
 void waterproof(IMAGE *cover, IMAGE *secret,int proof_len){
     if (cover->height != secret->height || cover->width != secret->width){
         printf("The 2 pictures you have entered do not have the same dimensions.\n");
@@ -126,6 +128,129 @@ void change_pixels(IMAGE *cover, IMAGE *secret,unsigned int proof_len) {
 }
 
 
+int getBit(char *m, int n){
+    if(n>=0 && n<=8*strlen(m)) {
+        int i = n / 8;
+        char ch = m[i];
+        int bitStatus;
+        bitStatus = (ch >> 7 - (n % 8)) & 1;
+        return bitStatus;
+    }
+    return 0;
+}
+
+int *createPermutationFunction(int n, unsigned int system_key){
+    srand(system_key);
+    int temp;
+    int *arr = calloc(n,sizeof(int));
+    // fill array with values
+    for (int i = 0; i < n; i++) {
+        arr[i] = i;
+    }
+    for (int i = 0; i < n; i++) {
+       int a = rand() % n;
+       int b = rand() % n;
+       temp = arr[a];
+       arr[a] = arr[b];
+       arr[b] = temp;
+    }
+    return arr;
+}
+
+void putTextInPicture(IMAGE *img, char *text, unsigned int system_key){
+   unsigned char c = 'a';
+    int ii = 0;
+    while (c != '\0'){
+        c = text[ii];
+
+        printf("%d [%c]\n",ii,c);
+        ii++;
+    }
+
+
+
+
+
+
+    unsigned char *new_pixel_array = calloc(img->pixel_array_size,1);
+    memcpy(new_pixel_array,img->pixel_array,img->pixel_array_size);
+    int b,o,n = (1+strlen(text))*8,*permutations = createPermutationFunction(n,system_key);
+    printf("arr is: %d and len is %d\n",n,strlen(text));
+    for (int i = 0; i < (1+strlen(text))*8; i++) {
+        b = getBit(text,i);
+        o = permutations[i];
+//        printf("%d\n",o);
+        new_pixel_array[o] = new_pixel_array[o] & 254;
+        new_pixel_array[o] = new_pixel_array[o] | b;
+    }
+    IMAGE new;
+    new.padding = img->padding;
+    new.row_length = img->row_length;
+    new.pixel_array_size = img->pixel_array_size;
+    new.width = img->width;
+    new.height = img->height;
+    new.bbp = img->bbp;
+    new.pixel_array = new_pixel_array;
+    // Save file
+    FILE *file = fopen("withEncodedText.bmp", "wb");
+    fwrite(img->file_head, sizeof(FILE_HEADER), 1, file);
+    fwrite(img->info_head, sizeof(INFO_HEADER), 1, file);
+    fwrite(&new_pixel_array[0], 1, new.pixel_array_size, file);
+    fclose(file);
+}
+
+char *readTextFromFile(char *filename){
+    char c,*text;
+    int index = 0;
+    FILE * fp;
+    fp = fopen (filename, "r");
+    if (fp==NULL){
+        printf("No such file.");
+        return NULL;
+    }
+    text = (char *)calloc(MAX_WORDS,sizeof(char));
+    while ((c = fgetc(fp)) != EOF){
+        text[index] = c;
+        index++;
+    }
+    fclose(fp);
+//    printf("%s\n",text);
+    // We know that im getting the text correctly
+    return text;
+}
+
+char *decodeTextFromImage(IMAGE *img,int textLen,unsigned int system_key){
+    char *text = calloc(textLen,sizeof(char));
+    int counter = 7,text_counter = 0, character_sum = 0;
+    int o = 0,n = (1+textLen)*8,*permutations = createPermutationFunction(n,system_key);
+    printf("arr is: %d and len is %d\n",n,textLen);
+
+    printf("\n\n\n------------------------------------\n\n\n");
+    for (int i = 0; i < (1+textLen)*8; i++) {
+        o = permutations[i];
+//        printf("%d\n",o);
+        int aa = img->pixel_array[o] & 1;
+//        printf("%d",aa);
+        aa = aa * pow(2,counter);
+        character_sum+=aa;
+
+        if (counter == 0) {
+//            printf("\n");
+            text[text_counter] = character_sum +'0';
+            text_counter++;
+            character_sum = 0;
+            counter = 7;
+        }
+        else counter--;
+//        b = getBit(text,i);
+//        o = permutations[i];
+//        img->pixel_array[o] = img->pixel_array[o] & 254;
+//        img->pixel_array[o] = img->pixel_array[o] & b;
+    }
+    return text;
+}
+
+
 void decode_image(IMAGE *img,unsigned int proof_len){
     unsigned char *new_pixel_array = calloc(img->pixel_array_size,1);
     int new_arr_length = img->pixel_array_size;
@@ -232,16 +357,7 @@ void createGrayscale(IMAGE *img) {
     fclose(file);
 }
 
-int getBit(char *m, int n){
-    if(n>=0 && n<=8*strlen(m)) {
-        int i = n / 8;
-        char ch = m[i];
-        int bitStatus;
-        bitStatus = (ch >> 7 - (n % 8)) & 1;
-        return bitStatus;
-    }
-    return 0;
-}
+
 
 void stringToImage(IMAGE *img, char *textFile){
     int bits[img->height * img->width];
@@ -356,14 +472,19 @@ void stringToImage(IMAGE *img, char *textFile){
 }
 
 int main(){
-    //01100011 01100001 01101110
-    for (int i = 0; i < 24; ++i) {
-        if(i%8==0)printf("\n");
-        printf("%d",getBit("can", i));
-    }
-
-    IMAGE *test =  load_bmp("tux-pirate.bmp");
-    stringToImage( test,"poem.txt");
+//    //01100011 01100001 01101110
+//    for (int i = 0; i < 24; ++i) {
+//        if(i%8==0)printf("\n");
+//        printf("%d",getBit("can", i));
+//    }
+    IMAGE *image =  load_bmp("tux-bonaparte.bmp");
+    char *text = readTextFromFile("poem.txt");
+    putTextInPicture(image,text,69);
+    IMAGE *with_text =  load_bmp("withEncodedText.bmp");
+    char *decoded_text = decodeTextFromImage(with_text,285,69);
+    printf("%s\n",decoded_text);
+//    IMAGE *test =  load_bmp("tux-pirate.bmp");
+//    stringToImage( test,"poem.txt");
     //IMAGE *test1 =  load_bmp("4x3.bmp");
    // print_information(test1);
     //IMAGE *test2 =  load_bmp("image2.bmp");
